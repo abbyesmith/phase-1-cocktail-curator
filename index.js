2529a3f70cfbf8351ff1d78ba6f6f8c6b927d79b
Sarah Whalley <s.whalley1130@gmail.com>
2023-02-10 14:56:13 -0700

    Adds function to display list of recipes

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 fetch("http://localhost:3000/menu")
  .then(response => response.json())
+ .then(menuData=>{
+    displayTitles(menuData);
+ })
 
  //TEST HERE
+
+ //function to get list of recipes
+
+ function displayTitles(menuData){
+    menuData.forEach(recipe=>{
+        let li=document.createElement('li');
+        li.textContent=recipe.name;
+        document.querySelector('#recipes').append(li);
+        li.addEventListener('click',()=>getCard(recipe))
+    })
+ }
+
+//function to display recipe card
+
+function getCard(recipe){
+    document.querySelector('h4').textContent=recipe.name;
+}
